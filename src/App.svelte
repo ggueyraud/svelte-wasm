85653dabd5af2e087c8054dea4c4d124f8fc602f
Guillaume <guillaumegueyraud@protonmail.com>
2020-04-11 15:15:18 +0200

    Init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { onMount } from 'svelte'
+
+	onMount(async () => {
+		const wasm = await import('../pkg')
+		wasm.greet('world')
+	})
+
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
